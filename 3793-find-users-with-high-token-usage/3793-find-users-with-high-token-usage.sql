139679538e61e024755ebdcee35fa34dd3d904b2
Hemasree Uppaluri Venkata Syamala <165351534+hemasreeuvs@users.noreply.github.com>
2026-02-05 15:16:31 -0600

    Time: 178 ms (60.51%), Space: 0B (100%) - LeetHub

---
+++ 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+
+SELECT user_id, COUNT(prompt) prompt_count, ROUND(AVG(tokens),2) avg_tokens
+FROM prompts
+GROUP BY user_id
+HAVING COUNT(prompt) > 2 AND MAX(tokens) > AVG(tokens)
+ORDER BY avg_tokens DESC, user_id;
